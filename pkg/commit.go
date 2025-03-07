3c14c95d7a1435e8eac72476a5b3aebcadcfe843
Ondrej Honus - Laptop <ondrejhonus@proton.me>
2025-03-07 15:44:01 +0100

    Move commit and createRepo from main

--- pkg/commit.go
+++ pkg/commit.go
@@ -1,1 +1,82 @@
 package pkg
+
+import (
+	"bubblegit/utils"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+///////////////////////////////////
+/////////// COMMIT ////////////////
+///////////////////////////////////
+
+// Get keypresses and update the commit message
+func TypeCommitMessage(m utils.Model, msg tea.Msg) (utils.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
+		case "ctrl+s", "enter":
+			// Commit when message is entered
+			if m.CommitMessage == "" {
+				m.StatusMessage = "Commit message cannot be empty!"
+				m.State = "status"
+				return m, nil
+			}
+			output := utils.RunCommand("git", "commit", "-m", m.CommitMessage)
+			if m.CommitDesc != "" {
+				output = utils.RunCommand("git", "commit", "-m", m.CommitMessage, "-m", m.CommitDesc)
+			}
+			m.StatusMessage = output
+			m.State = "status"
+			m.CommitMessage = ""
+			m.CommitDesc = ""
+		case "ctrl+d":
+			m.State = "commitDesc"
+		case "backspace":
+			// Handle backspace for commit message
+			if len(m.CommitMessage) > 0 {
+				m.CommitMessage = m.CommitMessage[:len(m.CommitMessage)-1]
+			}
+			// Handle backspace for commit description
+			if len(m.CommitDesc) > 0 {
+				m.CommitDesc = m.CommitDesc[:len(m.CommitDesc)-1]
+			}
+		case "ctrl+c":
+			// Handle exit to menu and clear both fields
+			m.State = "menu"
+			m.CommitMessage = ""
+			m.CommitDesc = ""
+		default:
+			m.CommitMessage += keyMsg.String()
+		}
+	}
+	return m, nil
+}
+
+// Get keypresses and update the commit description
+func TypeCommitDesc(m utils.Model, msg tea.Msg) (utils.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
+		case "ctrl+s", "enter":
+			// Commit when description is entered
+			output := utils.RunCommand("git", "commit", "-m", m.CommitMessage, "-m", m.CommitDesc)
+			m.StatusMessage = output
+			m.State = "status"
+			m.CommitMessage = ""
+			m.CommitDesc = ""
+		case "backspace":
+			// Handle backspace for commit description
+			if len(m.CommitDesc) > 0 {
+				m.CommitDesc = m.CommitDesc[:len(m.CommitDesc)-1]
+			}
+		case "ctrl+c":
+			// Handle exit to menu and clear both fields
+			m.State = "menu"
+			m.CommitMessage = ""
+			m.CommitDesc = ""
+		default:
+			// Append input to commit description
+			m.CommitDesc += keyMsg.String()
+		}
+	}
+	return m, nil
+}
--- pkg/createRepo.go
+++ pkg/createRepo.go
@@ -1,1 +1,305 @@
+/* Some gh commands ill use:
+1. gh repo create <repo-name> --description "<repo-description>" --public --source .
+2. gh repo create <repo-name> --description "<repo-description>" --private --source .
+This one creates a local folder and makes a gh repo
+3. gh repo create my-project --public --clone
+*/
+
+/*
+gh repo create <repo-name> --description "<repo-description>" --? --source .
+>	Create repo from ./
+	>	Repo name: [Default: {wd}]
+		Repo description:
+		Source: [Default: .]
+		[*] Public
+		[ ] Readme
+		[ ] .gitignore
+
+gh repo create <repo-name> --description "<repo-description>" --public --clone
+	Create empty remote and clone it
+	>	Repo name:
+		Repo description
+		[*] Public
+		[ ] Readme
+		[ ] .gitignore
+	Create empty remote repo
+	>	Repo name:
+		Repo description
+		[*] Public
+		[ ] Readme
+		[ ] .gitignore
+*/
+
 package pkg
+
+import (
+	"bubblegit/utils"
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+///////////////////////////////////
+/////////// CREATE REPO ///////////
+///////////////////////////////////
+
+// Create repo menu 1
+func RepoCreate(m utils.Model, msg tea.Msg) (utils.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
+		case "enter":
+			switch m.Cursor {
+			case 0:
+				m.State = "fromLocal"
+				m.Cursor = 0
+			case 1:
+				m.State = "createEmpty"
+				m.Cursor = 0
+			}
+		case "up", "k":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
+		case "down", "j":
+			if m.Cursor < 2 {
+				m.Cursor++
+			}
+		case "ctrl+c", "q":
+			m.State = "menu"
+		}
+	}
+	return m, nil
+}
+
+func ShowCreateRepoMenu(m utils.Model) string {
+	s := "What would you want to do?\n\n"
+	createChoices := []string{"Create repo from ./", "Create empty remote"}
+	for i, choice := range createChoices {
+		cursor := " "
+		if m.Cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	s += "\nPress [ctrl+c] to cancel, press [enter] to confirm.\n"
+	return s
+}
+
+// Get keypresses and update the file name to add
+func FromLocal(m utils.Model, msg tea.Msg) (utils.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
+		case "enter":
+			switch m.Cursor {
+			case 0:
+				// Repo name
+				if m.RepoName == "" {
+					m.RepoName = "bubblegit-repo"
+				}
+				m.Cursor++
+			case 1:
+				// Repo description
+				m.Cursor++
+			case 2:
+				// Source
+				if m.Source == "" {
+					m.Source = "."
+				}
+				m.Cursor++
+			case 3:
+				// Public
+				m.IsPublic = !m.IsPublic
+				m.Cursor++
+			case 4:
+				// Create repo
+				var visibility string
+				if m.IsPublic {
+					visibility = "--public"
+				} else {
+					visibility = "--private"
+				}
+				utils.RunCommand("gh", "repo", "create", m.RepoName, "--description", m.RepoDesc, visibility, "--source", m.Source)
+				m.State = "menu"
+				m.RepoName = ""
+				m.RepoDesc = ""
+				m.Source = ""
+				m.IsPublic = false
+			}
+		case "up":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
+		case "down", "tab":
+			if m.Cursor < 4 {
+				m.Cursor++
+			}
+		case "ctrl+c":
+			m.State = "menu"
+			m.RepoName = ""
+			m.RepoDesc = ""
+			m.Source = ""
+			m.IsPublic = false
+
+		case "backspace":
+			switch m.Cursor {
+			case 0:
+				if len(m.RepoName) > 0 {
+					m.RepoName = m.RepoName[:len(m.RepoName)-1]
+				}
+			case 1:
+				if len(m.RepoDesc) > 0 {
+					m.RepoDesc = m.RepoDesc[:len(m.RepoDesc)-1]
+				}
+			case 2:
+				if len(m.Source) > 0 {
+					m.Source = m.Source[:len(m.Source)-1]
+				}
+			}
+		default:
+			switch m.Cursor {
+			case 0:
+				m.RepoName += keyMsg.String()
+			case 1:
+				m.RepoDesc += keyMsg.String()
+			case 2:
+				m.Source += keyMsg.String()
+			}
+		}
+	}
+	return m, nil
+}
+
+func CreateEmpty(m utils.Model, msg tea.Msg) (utils.Model, tea.Cmd) {
+	if keyMsg, ok := msg.(tea.KeyMsg); ok {
+		switch keyMsg.String() {
+		case "enter":
+			switch m.Cursor {
+			case 0:
+				// Repo name
+				if m.RepoName == "" {
+					m.RepoName = "bubblegit-repo"
+				}
+				m.Cursor++
+			case 1:
+				// Repo description
+				m.Cursor++
+			case 2:
+				// Public?
+				m.IsPublic = !m.IsPublic
+				m.Cursor++
+			case 3:
+				// Clone?
+				m.CreateClone = !m.CreateClone
+				m.Cursor++
+			case 4:
+				// Create repo
+				var visibility string
+				if m.IsPublic {
+					visibility = "--public"
+				} else {
+					visibility = "--private"
+				}
+				var clone string
+				if m.CreateClone {
+					clone = "--clone"
+				} else {
+					clone = ""
+				}
+
+				// gh repo create <repo-name> --description "<repo-description>" --public --clone
+				utils.RunCommand("gh", "repo", "create", m.RepoName, "--description", m.RepoDesc, visibility, clone)
+				m.State = "menu"
+				m.RepoName = ""
+				m.RepoDesc = ""
+				m.Source = ""
+				m.IsPublic = false
+				m.CreateClone = false
+			}
+		case "up":
+			if m.Cursor > 0 {
+				m.Cursor--
+			}
+		case "down", "tab":
+			if m.Cursor < 4 {
+				m.Cursor++
+			}
+		case "ctrl+c":
+			m.State = "menu"
+			m.RepoName = ""
+			m.RepoDesc = ""
+			m.Source = ""
+			m.IsPublic = false
+
+		case "backspace":
+			switch m.Cursor {
+			case 0:
+				if len(m.RepoName) > 0 {
+					m.RepoName = m.RepoName[:len(m.RepoName)-1]
+				}
+			case 1:
+				if len(m.RepoDesc) > 0 {
+					m.RepoDesc = m.RepoDesc[:len(m.RepoDesc)-1]
+				}
+			case 2:
+				if len(m.Source) > 0 {
+					m.Source = m.Source[:len(m.Source)-1]
+				}
+			}
+		default:
+			switch m.Cursor {
+			case 0:
+				m.RepoName += keyMsg.String()
+			case 1:
+				m.RepoDesc += keyMsg.String()
+			case 2:
+				m.Source += keyMsg.String()
+			}
+		}
+	}
+	return m, nil
+}
+
+func ShowCreateFromLocal(m utils.Model) string {
+	s := "Enter the following details:\n\n"
+	createChoices := []string{
+		fmt.Sprintf("Name: %s", m.RepoName),
+		fmt.Sprintf("Description: %s", m.RepoDesc),
+		fmt.Sprintf("Source (default = ./): %s", m.Source),
+		fmt.Sprintf("Public: %t", m.IsPublic),
+		"[Create repo]",
+	}
+
+	for i, choice := range createChoices {
+		cursor := " "
+		if m.Cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	s += "\nPress [ctrl+c] to cancel, press [enter] to toggle true/false.\n"
+	return s
+}
+
+func ShowCreateEmpty(m utils.Model) string {
+	s := "Enter the following details:\n\n"
+	createChoices := []string{
+		fmt.Sprintf("Name: %s", m.RepoName),
+		fmt.Sprintf("Description: %s", m.RepoDesc),
+		fmt.Sprintf("Public: %t", m.IsPublic),
+		fmt.Sprintf("Clone: %t", m.CreateClone),
+		"[Create repo]",
+	}
+
+	for i, choice := range createChoices {
+		cursor := " "
+		if m.Cursor == i {
+			cursor = ">"
+		}
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	s += "\nPress [ctrl+c] to cancel, press [enter] to toggle true/false.\n"
+	return s
+}
